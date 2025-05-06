0679812eb053282342ab31ab391c04c7d46ee0d7
MdSaifulIslamRafsan <mdsaifulislamrafsan099@gmail.com>
2025-05-06 16:11:31 +0600

    change Admin Overview page design

--- src/app/(dashboardLayout)/dashboard/page.tsx
+++ src/app/(dashboardLayout)/dashboard/page.tsx
@@ -1,6 +1,20 @@
 import ReviewPieChart from '@/components/dashboard/dashboardOverview';
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  BarChart,
+  Clock,
+  Star,
+  DollarSign,
+  Users,
+  LucideIcon,
+} from 'lucide-react';
 
+interface cardType  {
+  title: string;
+  value: string | number;
+  icon: LucideIcon;
+  description: string;
+}
 type DashboardData = {
   totalReviews: number;
   totalPendingReviews: number;
@@ -28,42 +42,54 @@ async function getDashboardData(): Promise<DashboardData> {
 export default async function AdminDashboardPage() {
   const data = await getDashboardData();
 
-  const cards = [
+  const cards: cardType[] = [
     {
       title: 'Total Reviews',
       value: data.totalReviews,
+      icon: BarChart,
+      description: 'All reviews in the system',
     },
     {
       title: 'Pending Reviews',
       value: data.totalPendingReviews,
+      icon: Clock,
+      description: 'Reviews awaiting approval',
     },
     {
       title: 'Premium Reviews',
       value: data.totalPremiumReviews,
+      icon: Star,
+      description: 'Paid premium reviews',
     },
     {
       title: 'Total Payments',
       value: `$${data.totalPayments.toFixed(2)}`,
+      icon: DollarSign,
+      description: 'Total payment amount',
     },
     {
       title: 'Total Users',
       value: data.totalUsers,
+      icon: Users,
+      description: 'Registered users',
     },
   ];
   
   return (
     <div className="p-6">
       <h2 className="text-3xl font-bold text-primary mb-6">Admin Overview</h2>
-      <div className="grid grid-cols-1 text-center sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-4  gap-6">
         {cards.map((card, idx) => (
-          <Card key={idx} className={`bg-primary/5 shadow-lg rounded-2xl`}>
-            <CardContent className="p-5">
-              <h3 className="text-lg font-medium text-gray-600">
-                {card.title}
-              </h3>
-              <p className="text-2xl font-bold mt-2">{card.value}</p>
-            </CardContent>
-          </Card>
+         <Card key={idx} className="border-green-100 gap-0">
+         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+           <CardTitle className="text-lg font-medium">{card?.title}</CardTitle>
+           <card.icon className="h-4 w-4 text-green-600" />
+         </CardHeader>
+         <CardContent>
+           <div className="text-2xl font-bold">{card.value}</div>
+           <p className="text-xs text-muted-foreground">{card.description}</p>
+         </CardContent>
+       </Card>
         ))}
       </div>
       <ReviewPieChart
