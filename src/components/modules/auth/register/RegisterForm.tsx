269f8164fb2c0c47b28ce9595071834cfad1a3ef
msetu1 <msetu5763@gmail.com>
2025-05-09 11:02:44 +0600

    register form and validation schema done

---
+++ src/components/modules/auth/register/RegisterForm.tsx
@@ -0,0 +1,125 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+const RegisterForm = () => {
+    const form = useForm()
+    const onSubmit:SubmitHandler<FieldValues>=(data)=>{
+console.log(data)
+    }
+    return (
+  //       <div className="max-w-md ">
+  //         <Form {...form}>
+  //           <form onSubmit={form.handleSubmit(onSubmit)}>
+  //           <FormField
+  //       control={form.control}
+  //       name="..."
+  //       render={({field}) => (
+  //     <FormItem>
+  //       <FormLabel />
+  //       <FormControl>
+  //       <Input {...field}  value={field.value || ""} />
+  //       </FormControl>
+  //       <FormMessage />
+  //     </FormItem>
+  //   )}
+  // />
+  // <Button type="submit">Register</Button>
+  //           </form>
+  //         </Form>
+
+  //       </div>
+  <div className="border-2 border-gray-300 rounded-xl flex-grow max-w-md w-full p-5 mx-auto">
+      <div className="flex items-center space-x-4 ">
+        {/* <Logo /> */}
+        <div>
+          <h1 className="text-xl font-semibold text-center">Register Now</h1>
+          {/* <p className="font-extralight text-sm text-gray-600">
+            Join us today and start your journey!
+          </p> */}
+        </div>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="passwordConfirm"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} value={field.value || ""} />
+                </FormControl>
+
+                {/* {passwordConfirm && password !== passwordConfirm ? (
+                  <FormMessage> Password does not match </FormMessage>
+                ) : (
+                  <FormMessage />
+                )} */}
+              </FormItem>
+            )}
+          />
+
+          <Button
+            // disabled={passwordConfirm && password !== passwordConfirm}
+            type="submit"
+            className="mt-5 w-full"
+          >
+            {/* {isSubmitting ? "Registering...." : "Register"} */}
+            Register
+          </Button>
+        </form>
+      </Form>
+      <p className="text-sm text-gray-600 text-center my-3">
+        Already have an account ?
+        <Link href="/login" className="text-primary">
+          Login
+        </Link>
+      </p>
+    </div>
+    );
+};
+
+export default RegisterForm;
