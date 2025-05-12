51c42bcdab7f2672a2be1219eab50c992423357e
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2025-05-12 23:50:08 +0600

    added fixed error

--- src/app/(mainLayout)/checkout/[reviewId]/page.tsx
+++ src/app/(mainLayout)/checkout/[reviewId]/page.tsx
@@ -1,10 +1,11 @@
 import CheckoutPage from "@/components/Checkout/CheckoutPage";
 import { getReviewById } from "@/services/review";
+import { Review } from "@/types/reviewTypes";
 import React from "react";
 type Params = Promise<{ reviewId: string }>;
 const ReviewCheckout = async ({ params }: { params: Params }) => {
   const { reviewId } = await params;
-  const reviews = await getReviewById(reviewId);
+  const reviews: Review = await getReviewById(reviewId);
   return (
     <div>
       <CheckoutPage reviews={reviews} />
